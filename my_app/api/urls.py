d4262c6c54b8c8e9fb415dae855dcea8089f4836
pd-shah <pd.shahsafi@gmail.com>
2018-03-07 15:43:25 +0330

    3 finished

--- my_app/api/urls.py
+++ my_app/api/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import url
 
-from . import views
-
 from rest_framework_swagger.views import get_swagger_view
+
+from . import views
 
 swagger = get_swagger_view(title='Public API')
 
 urlpatterns = [
-    url(r'^test/$', view=views.test, name='test'),
+
     url(
         r'^author/$',
-        view=views.AuthorListApiView.as_view(),
+        view=views.AuthorList.as_view(),
         name='author-list-api-view'
     ),
+
+    url(
+        r'^author/(?P<pk>[0-9]+)$',
+        views.AuthorDetail.as_view(),
+        name='author_detail-api-view'
+    ),
+
     url(r'^swagger/$', view=swagger, name='swagger'),
 ]
--- my_app/api/views.py
+++ my_app/api/views.py
@@ -1,17 +1,114 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from rest_framework import generics
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import serializers
 from my_app import models
 
+# def test(request, ):
+#     return HttpResponse('well done!')
+#
+#
+# class AuthorListApiView(generics.ListAPIView):
+#     serializer_class = serializers.AuthorSerializer
+#
+#     def get_queryset(self, ):
+#         return models.Author.objects.all()
 
-def test(request, ):
-    return HttpResponse('well done!')
+
+# @api_view(['GET', 'POST'])
+# def author_list(request):
+#     """
+#     List all code Authors, or create a new author.
+#     """
+#     if request.method == 'GET':
+#         authors = models.Author.objects.all()
+#         serializer = serializers.AuthorSerializer(authors, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = serializers.AuthorSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def author_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code author.
+#     """
+#     try:
+#         author = models.Author.objects.get(pk=pk)
+#     except models.Author.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         serializer = serializers.AuthorSerializer(author)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'PUT':
+#         serializer = serializers.AuthorSerializer(author, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         author.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AuthorList(generics.GenericAPIView):
+    """
+    List all authors, or create a new Author.
+    """
+
+    serializer_class = serializers.AuthorSerializer
+    
+    def get(self, request, format=None):
+        authors = models.Author.objects.all()
+        serializer = serializers.AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = serializers.AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AuthorListApiView(generics.ListAPIView):
+class AuthorDetail(generics.GenericAPIView):
+    """
+    Retrieve, update or delete a author instance.
+    """
+
     serializer_class = serializers.AuthorSerializer
 
-    def get_queryset(self, ):
-        return models.Author.objects.all()
+    def get_object(self, pk):
+        try:
+            return models.Author.objects.get(pk=pk)
+        except models.Author.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = serializers.AuthorSerializer(author)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = serializers.AuthorSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
